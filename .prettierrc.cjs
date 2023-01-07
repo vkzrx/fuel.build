36e85715c10629b3686fd1a467743ce50b71d25d
0xYami <0xYami@proton.me>
2023-01-07 22:16:38 +0100

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  trailingComma: "all",
+  singleQuote: false,
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+        astroAllowShorthand: true,
+      },
+    },
+  ],
+};
