de66436800381920ee94942aa0a7deae52d9ee17
0xYami <118700396+0xYami@users.noreply.github.com>
2023-01-09 14:48:08 +0100

    feat: add projects (#2)

--- data/projects.ts
+++ data/projects.ts
@@ -1,27 +1,203 @@
-import { socials } from "~/constants";
 import type { Project } from "~/types";
 
 export const projects: Project[] = [
   {
-    name: "fuel.build",
+    name: "Microchain",
     description:
-      "Explore and join the world's fastest modular execution layer ecosystem",
-    href: "/",
-    logoURL: "/logos/sparky.svg",
-    coverURL: "/covers/sparky.png",
-    domains: ["DeFi", "NFT", "Marketplace"],
+      "A low-cost, easy-to-use token swapping terminal with two major benefits... versatility and simplicity",
+    href: "https://microchain.systems/",
+    logoURL: "/logos/microchain.jpeg",
+    coverURL: "/covers/microchain.jpeg",
+    domains: ["DeFi"],
     socials: [
       {
         type: "twitter",
-        link: socials.yami.twitter,
-      },
-      {
-        type: "github",
-        link: socials.yami.github,
+        link: "https://twitter.com/MicrochainSys",
       },
       {
         type: "website",
-        link: "https://fuel.build",
+        link: "https://microchain.systems/",
+      },
+    ],
+  },
+  {
+    name: "Yama Finance",
+    description: "Novel Omnichain Stablecoin",
+    href: "https://twitter.com/YamaFinance",
+    logoURL: "/logos/yama-finance.jpeg",
+    coverURL: "/covers/yama-finance.jpeg",
+    domains: ["DeFi", "Bridge"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/YamaFinance",
+      },
+    ],
+  },
+  {
+    name: "Poolshark",
+    description: "A decentralized protocol for directional liquidity",
+    href: "https://docs.poolsharks.io",
+    logoURL: "/logos/poolshark.jpeg",
+    coverURL: "/covers/poolshark.jpeg",
+    domains: ["DeFi"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/poolsharks_labs",
+      },
+      {
+        type: "discord",
+        link: "https://discord.com/invite/HJhrD9sZ6K",
+      },
+      {
+        type: "github",
+        link: "https://github.com/poolsharks-protocol",
+      },
+      {
+        type: "website",
+        link: "https://docs.poolsharks.io",
+      },
+    ],
+  },
+  {
+    name: "Elix",
+    description:
+      "Hybrid automated market maker featuring automated concentrated liquidity and delivering additional capital efficiency build on @fuellabs_",
+    href: "https://elix.finance",
+    logoURL: "/logos/elix.png",
+    coverURL: "/covers/elix.jpeg",
+    domains: ["DeFi"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/elixfinance",
+      },
+      {
+        type: "website",
+        link: "https://elix.finance",
+      },
+    ],
+  },
+  {
+    name: "Thunder",
+    description: "Building the first NFT marketplace on @fuellabs_",
+    href: "https://thundernft.market",
+    logoURL: "/logos/thunder.jpeg",
+    coverURL: "/covers/thunder.jpeg",
+    domains: ["NFT", "Marketplace"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/ThunderbyFuel",
+      },
+      {
+        type: "website",
+        link: "https://thundernft.market/",
+      },
+    ],
+  },
+  {
+    name: "Webgum",
+    description:
+      "🕸️ An online marketplace where you can buy and sell encrypted files",
+    href: "https://twitter.com/webgumapp",
+    logoURL: "/logos/webgum.jpeg",
+    domains: ["Marketplace"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/webgumapp",
+      },
+      {
+        type: "github",
+        link: "https://github.com/Webgum",
+      },
+    ],
+  },
+  {
+    name: "Unic",
+    description: "Verify your NFT by scanning",
+    href: "https://unic.io/",
+    logoURL: "/logos/unic.jpeg",
+    coverURL: "/covers/unic.jpeg",
+    domains: ["NFT", "Marketplace"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/0xUNIC",
+      },
+      {
+        type: "website",
+        link: "https://unic.io/",
+      },
+    ],
+  },
+  {
+    name: "Fuelet",
+    description:
+      "Self-custodial wallet for the next billion users - Soon on iOS, Android and Chrome",
+    href: "https://fuelet.app/",
+    logoURL: "/logos/fuelet.png",
+    coverURL: "/covers/fuelet.jpeg",
+    domains: ["Wallet"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/FueletWallet",
+      },
+      {
+        type: "website",
+        link: "https://fuelet.app",
+      },
+    ],
+  },
+  {
+    name: "Fuel Nomen",
+    description: "The next generation blockchain name system",
+    href: "https://www.nomen.sh",
+    logoURL: "/logos/fuel-nomen.jpeg",
+    domains: ["Identity"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/fuelnomen",
+      },
+      {
+        type: "website",
+        link: "https://www.nomen.sh",
+      },
+    ],
+  },
+  {
+    name: "Fuel Punks",
+    description:
+      "1111 dystopian punks ready to conquer the Fastest Modular Execution Layer ",
+    href: "https://twitter.com/FuelPunks",
+    logoURL: "/logos/fuel-punks.jpeg",
+    coverURL: "/covers/fuel-punks.jpeg",
+    domains: ["NFT", "Marketplace"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/FuelPunks",
+      },
+    ],
+  },
+  {
+    name: "Orao",
+    description: "Oracle service for custom data feeds",
+    href: "https://orao.network/",
+    logoURL: "/logos/orao.jpeg",
+    domains: ["NFT", "Marketplace"],
+    socials: [
+      {
+        type: "twitter",
+        link: "https://twitter.com/OraoNetwork",
+      },
+      {
+        type: "website",
+        link: "https://orao.network/",
       },
     ],
   },
